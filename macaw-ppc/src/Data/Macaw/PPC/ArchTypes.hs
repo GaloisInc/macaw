90a9c5ff6502d5ee6d9e4234b6171c42f8d4586e
Tristan Ravitch <tristan@nochair.net>
2017-10-02 11:44:46 -0700

    Generalize the architecture info def
    Now we can use the same infrastructure for PPC32 and PPC64.  There might be some
    rough edges, but we can work around those with type functions.
    
    This change also adds an implementation for the preserved registers across
    system calls for macaw.

--- macaw-ppc/src/Data/Macaw/PPC/ArchTypes.hs
+++ macaw-ppc/src/Data/Macaw/PPC/ArchTypes.hs
@@ -7,6 +7,7 @@ module Data.Macaw.PPC.ArchTypes
 import Data.Macaw.CFG
 import Data.Macaw.Types
 
+import qualified SemMC.Architecture.PPC32 as PPC32
 import qualified SemMC.Architecture.PPC64 as PPC64
 
 import Data.Macaw.PPC.PPCReg
@@ -15,4 +16,5 @@ import Data.Macaw.PPC.PPCReg
 ------------------------------------------------------------------------
 -- PPC specific declarations
 
-type instance ArchReg PPC64.PPC = PPCReg
+type instance ArchReg PPC64.PPC = PPCReg PPC64.PPC
+type instance ArchReg PPC32.PPC = PPCReg PPC32.PPC
