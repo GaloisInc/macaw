07ff647dcfd150593eacd9efbe5f72e0a089c77d
Tristan Ravitch <tristan@nochair.net>
2017-10-02 09:40:17 -0700

    Add some missing instances for PPCReg

--- macaw-ppc/src/Data/Macaw/PPC/PPCReg.hs
+++ macaw-ppc/src/Data/Macaw/PPC/PPCReg.hs
@@ -1,18 +1,31 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
-
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Data.Macaw.PPC.PPCReg where
 
 import Data.Macaw.Types
 import Data.Parameterized.Classes
 import Data.Parameterized.Some
+import qualified Data.Parameterized.TH.GADT as TH
 import qualified Dismantle.PPC as D
 
 data PPCReg tp where
   PPC_GP :: D.GPR -> PPCReg (BVType 64)
+
+deriving instance Eq (PPCReg tp)
+deriving instance Ord (PPCReg tp)
 
 instance Show (PPCReg tp) where
   show (PPC_GP r) = show r
 
 instance ShowF PPCReg where
   showF = show
+
+$(return [])
+
+instance TestEquality PPCReg where
+  testEquality = $(TH.structuralTypeEquality [t| PPCReg |] [])
+
+instance OrdF PPCReg where
+  compareF = $(TH.structuralTypeOrd [t| PPCReg |] [])
