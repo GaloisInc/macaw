0a974b1a15b933b57b1de652bbd4cfd06c205e6f
Tristan Ravitch <tristan@nochair.net>
2017-10-04 16:00:18 -0700

    Add the TH invocation to PPC32

--- macaw-ppc/src/Data/Macaw/PPC/Semantics/PPC32.hs
+++ macaw-ppc/src/Data/Macaw/PPC/Semantics/PPC32.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Data.Macaw.PPC.Semantics.PPC32
   ( execInstruction
   ) where
 
 import qualified Dismantle.PPC as D
+import qualified Data.Parameterized.Map as MapF
 import qualified Data.Macaw.CFG as MC
 import qualified Data.Macaw.Types as MT
 import           SemMC.Architecture.PPC32 ( PPC )
 
 import           Data.Macaw.PPC.Generator
+import           Data.Macaw.PPC.Semantics.TH ( genExecInstruction )
 
 execInstruction :: MC.Value ppc s (MT.BVType 32) -> D.Instruction -> Maybe (PPCGenerator PPC s ())
-execInstruction = undefined
+execInstruction =  $(genExecInstruction MapF.empty)
