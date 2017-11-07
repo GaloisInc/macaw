e405e23b9337fb3e90b12d249d510953e13af0c8
Tristan Ravitch <tristan@nochair.net>
2017-11-07 11:24:57 -0800

    Fix a bug where we were generating macaw bitvector literals with negative integer components
    Apparently this is not allowed, and caused an assertion failure in a pretty printer.

--- macaw-ppc/src/Data/Macaw/PPC/Operand.hs
+++ macaw-ppc/src/Data/Macaw/PPC/Operand.hs
@@ -17,6 +17,7 @@ module Data.Macaw.PPC.Operand (
 
 import           GHC.TypeLits
 import           Data.Int ( Int16 )
+import           Data.Word ( Word32 )
 import qualified Data.Parameterized.NatRepr as NR
 import qualified Data.Macaw.CFG.Core as MC
 import           Data.Macaw.Types
@@ -57,34 +58,34 @@ instance ExtractValue ppc D.FR (BVType 128) where
   extractValue fr = G.getRegValue (R.PPC_FR fr)
 
 instance ExtractValue arch D.AbsBranchTarget (BVType 24) where
-  extractValue (D.ABT w) = return $ MC.BVValue NR.knownNat (fromIntegral w)
+  extractValue (D.ABT w) = return $ MC.BVValue NR.knownNat (toIntegerWord w)
 
 instance ExtractValue arch D.CondBranchTarget (BVType 14) where
-  extractValue (D.CBT i) = return $ MC.BVValue NR.knownNat (fromIntegral i)
+  extractValue (D.CBT i) = return $ MC.BVValue NR.knownNat (toIntegerWord i)
 
 instance ExtractValue arch D.AbsCondBranchTarget (BVType 14) where
-  extractValue (D.ACBT w) = return $ MC.BVValue NR.knownNat (fromIntegral w)
+  extractValue (D.ACBT w) = return $ MC.BVValue NR.knownNat (toIntegerWord w)
 
 instance ExtractValue arch D.BranchTarget (BVType 24) where
-  extractValue (D.BT i) = return $ MC.BVValue NR.knownNat (fromIntegral i)
+  extractValue (D.BT i) = return $ MC.BVValue NR.knownNat (toIntegerWord i)
 
 instance (KnownNat n, 1 <= n) => ExtractValue arch (I.I n) (BVType n) where
-  extractValue (I.I i) = return $ MC.BVValue NR.knownNat (fromIntegral i)
+  extractValue (I.I i) = return $ MC.BVValue NR.knownNat (toIntegerWord i)
 
 instance (KnownNat n, 1 <= n) => ExtractValue arch (W.W n) (BVType n) where
-  extractValue w = return $ MC.BVValue NR.knownNat (fromIntegral (W.unW w))
+  extractValue w = return $ MC.BVValue NR.knownNat (toIntegerWord (W.unW w))
 
 instance ExtractValue arch Int16 (BVType 16) where
-  extractValue i = return $ MC.BVValue NR.knownNat (fromIntegral i)
+  extractValue i = return $ MC.BVValue NR.knownNat (toIntegerWord i)
 
 instance ExtractValue arch D.CRBitM (BVType 4) where
-  extractValue (D.CRBitM b) = return $ MC.BVValue NR.knownNat (fromIntegral b)
+  extractValue (D.CRBitM b) = return $ MC.BVValue NR.knownNat (toIntegerWord b)
 
 instance ExtractValue arch D.CRBitRC (BVType 5) where
-  extractValue (D.CRBitRC b) = return $ MC.BVValue NR.knownNat (fromIntegral b)
+  extractValue (D.CRBitRC b) = return $ MC.BVValue NR.knownNat (toIntegerWord b)
 
 instance ExtractValue arch D.CRRC (BVType 3) where
-  extractValue (D.CRRC b) = return $ MC.BVValue NR.knownNat (fromIntegral b)
+  extractValue (D.CRRC b) = return $ MC.BVValue NR.knownNat (toIntegerWord b)
 
 class ToPPCReg a arch tp | a arch -> tp where
   toPPCReg :: a -> R.PPCReg arch tp
@@ -97,3 +98,16 @@ instance ToPPCReg D.GPR PPC64.PPC (BVType 64) where
 
 instance ToPPCReg D.FR arch (BVType 128) where
   toPPCReg = R.PPC_FR
+
+-- | Convert to a positive integer through a word type
+--
+-- We convert through a word because the integer returned is not allowed to be
+-- negative.  Negative values must be converted to an unsigned word form, which
+-- we can then promote to Integer.
+--
+-- For PowerPC, Word32 is large enough to accommodate all literal values
+toIntegerWord :: (Integral a) => a -> Integer
+toIntegerWord i = toInteger w
+  where
+    w :: Word32
+    w = fromIntegral i
