fcb694ccb94835abc71737271e214fffd60878e1
Tristan Ravitch <tristan@nochair.net>
2017-11-06 17:19:16 -0800

    Add a test for conditional branches
    It doesn't pass yet.  It is hung up on an unsupported register type (Crrc, used
    in a compare).

--- macaw-ppc/tests/ppc/test-conditional.s
+++ macaw-ppc/tests/ppc/test-conditional.s
@@ -1,4 +1,11 @@
-	.file	"test-just-exit.c"
+	.file	"test-conditional.c"
+	.globl g
+	.section	".data"
+	.align 2
+	.type	g, @object
+	.size	g, 4
+g:
+	.long	-11
 	.section	".text"
 	.align 2
 	.globl _start
@@ -12,8 +19,22 @@ _start:
 	std 31,-8(1)
 	stdu 1,-64(1)
 	mr 31,1
+	addis 9,2,g@toc@ha
+	addi 9,9,g@toc@l
+	lwa 9,0(9)
+	cmpwi 7,9,0
+	ble 7,.L2
+	addis 9,2,g@toc@ha
+	addi 9,9,g@toc@l
+	lwa 9,0(9)
+	addi 9,9,1
+	extsw 10,9
+	addis 9,2,g@toc@ha
+	addi 9,9,g@toc@l
+	stw 10,0(9)
+.L2:
 #APP
- # 4 "test-just-exit.c" 1
+ # 10 "test-conditional.c" 1
 	li 0,1
 sc
  # 0 "" 2
