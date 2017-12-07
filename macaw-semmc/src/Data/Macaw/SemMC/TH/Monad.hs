7bcc90c83df5dd96d9f9ddc233fe6bff533b8986
Tristan Ravitch <tristan@nochair.net>
2017-12-06 17:28:23 -0800

    Start migrating shareable TH code from macaw-ppc to macaw-semmc
    This change still needs some work, but no other code depends on it yet

--- macaw-semmc/src/Data/Macaw/SemMC/TH/Monad.hs
+++ macaw-semmc/src/Data/Macaw/SemMC/TH/Monad.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 module Data.Macaw.SemMC.TH.Monad (
+  BoundVarInterpretations(..),
   MacawQ,
   runMacawQ,
   liftQ,
   lookupElt,
   appendStmt,
   withLocToReg,
+  withNonceAppEvaluator,
   bindExpr
   ) where
 
@@ -17,9 +19,22 @@ import qualified Data.Map.Strict as M
 import qualified Data.Sequence as Seq
 import           Language.Haskell.TH
 
+import           Data.Parameterized.FreeParamF ( FreeParamF(..) )
+import qualified Data.Parameterized.Map as Map
 import           Data.Parameterized.Some ( Some(..) )
+import qualified Lang.Crucible.Solver.Interface as SI
 import qualified Lang.Crucible.Solver.SimpleBuilder as S
+import qualified Lang.Crucible.Solver.SimpleBackend as S
+
 import qualified SemMC.Architecture.Location as L
+
+type Sym t = S.SimpleBackend t
+
+data BoundVarInterpretations arch t =
+  BoundVarInterpretations { locVars :: Map.MapF (SI.BoundVar (Sym t)) (L.Location arch)
+                          , opVars  :: Map.MapF (SI.BoundVar (Sym t)) (FreeParamF Name)
+                          , regsValName :: Name
+                          }
 
 data QState arch t = QState { accumulatedStatements :: !(Seq.Seq Stmt)
                             -- ^ The list of template haskell statements accumulated
@@ -31,12 +46,19 @@ data QState arch t = QState { accumulatedStatements :: !(Seq.Seq Stmt)
                             -- instead, they are names that are bound for those
                             -- terms (via 'VarE')
                             , locToReg :: forall tp . L.Location arch tp -> Q Exp
+                            , nonceAppEvaluator :: forall tp
+                                                 . BoundVarInterpretations arch t
+                                                -> S.NonceApp t (S.Elt t) tp
+                                                -> Maybe (MacawQ arch t Exp)
                             }
 
-emptyQState :: (forall tp . L.Location arch tp -> Q Exp) -> QState arch t
-emptyQState ltr = QState { accumulatedStatements = Seq.empty
+emptyQState :: (forall tp . L.Location arch tp -> Q Exp)
+            -> (forall tp . BoundVarInterpretations arch t -> S.NonceApp t (S.Elt t) tp -> Maybe (MacawQ arch t Exp))
+            -> QState arch t
+emptyQState ltr ena = QState { accumulatedStatements = Seq.empty
                          , expressionCache = M.empty
                          , locToReg = ltr
+                         , nonceAppEvaluator = ena
                          }
 
 newtype MacawQ arch t a = MacawQ { unQ :: St.StateT (QState arch t) Q a }
@@ -45,8 +67,11 @@ newtype MacawQ arch t a = MacawQ { unQ :: St.StateT (QState arch t) Q a }
             Monad,
             St.MonadState (QState arch t))
 
-runMacawQ :: (forall tp . L.Location arch tp -> Q Exp) -> MacawQ arch t () -> Q [Stmt]
-runMacawQ ltr act = (F.toList . accumulatedStatements) <$> St.execStateT (unQ act) (emptyQState ltr)
+runMacawQ :: (forall tp . L.Location arch tp -> Q Exp)
+          -> (forall tp . BoundVarInterpretations arch t -> S.NonceApp t (S.Elt t) tp -> Maybe (MacawQ arch t Exp))
+          -> MacawQ arch t ()
+          -> Q [Stmt]
+runMacawQ ltr ena act = (F.toList . accumulatedStatements) <$> St.execStateT (unQ act) (emptyQState ltr ena)
 
 -- | Lift a TH computation (in the 'Q' monad) into the monad.
 liftQ :: Q a -> MacawQ arch t a
@@ -62,6 +87,13 @@ lookupElt :: S.Elt t tp -> MacawQ arch t (Maybe Exp)
 lookupElt elt = do
   c <- St.gets expressionCache
   return (M.lookup (Some elt) c)
+
+withNonceAppEvaluator :: forall tp arch t
+                       . ((BoundVarInterpretations arch t -> S.NonceApp t (S.Elt t) tp -> Maybe (MacawQ arch t Exp)) -> MacawQ arch t (Maybe (MacawQ arch t Exp)))
+                      -> MacawQ arch t (Maybe (MacawQ arch t Exp))
+withNonceAppEvaluator k = do
+  nae <- St.gets nonceAppEvaluator
+  k nae
 
 -- | Append a statement that doesn't need to bind a new name
 appendStmt :: ExpQ -> MacawQ arch t ()
