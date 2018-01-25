dc1bfacfa5fa0a75ac8d6b09880d9e134c66295d
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-01-25 15:29:47 -0800

    Some more operations

--- x86_symbolic/src/Data/Macaw/X86/Semantics.hs
+++ x86_symbolic/src/Data/Macaw/X86/Semantics.hs
@@ -4,14 +4,17 @@
 {-# Language DataKinds #-}
 {-# Language TypeApplications #-}
 {-# Language TypeFamilies #-}
-{-# Language EmptyCase #-}
-{-# Language ScopedTypeVariables #-}
 {-# Language TypeOperators #-}
+{-# Language ScopedTypeVariables #-}
+{-# Language EmptyCase #-}
+{-# Language MultiWayIf #-}
+{-# Language PatternGuards #-}
 module Data.Macaw.X86.Semantics where
 
 import Data.Parameterized.NatRepr
 import Data.Bits(shiftR, (.&.))
 import Data.Word(Word8)
+import Data.Bits(shiftL)
 import GHC.TypeLits(KnownNat)
 
 import           Lang.Crucible.Simulator.ExecutionTree
@@ -51,31 +54,45 @@ pureSem sym fn =
 
     M.VOp1 w op1 x ->
       case op1 of
-        M.VShiftL n ->
-           unpack BigEndian w n8 x $ \vecIn ->
-           pack BigEndian sym (V.shiftL (fromIntegral n) zero vecIn)
+        M.VShiftL n -> vecOp1 sym BigEndian w n8 x
+                        (V.shiftL (fromIntegral n) (bv 0))
 
-        M.VShufD mask ->
-           unpack LittleEndian w n32 x $ \vecIn ->
-           pack LittleEndian sym (shuffle mask vecIn)
+        M.VShufD mask -> vecOp1 sym LittleEndian w n32 x (shuffle mask)
 
-{-
     M.VOp2 w op2 x y ->
       case op2 of
-        M.VPOr          -> evalE sym $ app $ BVOr  w (getVal x) (getVal y)
-        M.VPXor         -> evalE sym $ app $ BVXor w (getVal x) (getVal y)
-        M.VPAlignR i    ->
-          case testEquality w n128 of
-            Just Refl ->
-              unpack BigEndian w n8 x $ \xs ->
-              unpack BigEndian w n8 y $ \ys ->
-              pack BigEndian sym (vpalign i xs ys)
+        M.VPOr   -> bitOp2 sym x y (BVOr w)
+        M.VPXor  -> bitOp2 sym x y (BVXor w)
 
-        M.VPShufB       -> undefined
+        M.VPAlignR s -> vecOp2 sym BigEndian w n8 x y $ \xs ys ->
+          divExact (V.length xs) n16 $ \i ->
+              V.join n16 $ V.zipWith (vpalign s)
+                                     (V.split i n16 xs)
+                                     (V.split i n16 ys)
+
+        M.VPShufB -> vecOp2 sym LittleEndian w n8 x y $ \xs ys ->
+          divExact (V.length xs) n16 $ \i ->
+            V.join n16 $ V.zipWith shufb (V.split i n16 xs) (V.split i n16 ys)
+
+
+{-
         M.VAESEnc       -> undefined
         M.VAESEncLast   -> undefined
         M.VPCLMULQDQ i  -> undefined
 -}
+
+divExact ::
+  NatRepr n ->
+  NatRepr x ->
+  (forall i. ((i * x) ~ n, 1 <= i) => NatRepr i -> k) ->
+  k
+divExact n x k = withDivModNat n x $ \i r ->
+  case testEquality r n0 of
+    Just Refl ->
+      case testLeq n1 i of
+        Just LeqProof -> k i
+        Nothing       -> error "divExact: 0 input"
+    Nothing -> error "divExact: not a multiple of 16"
 
 -- | Assumes big-endian split
 vpalign :: Word8 ->
@@ -83,7 +100,7 @@ vpalign :: Word8 ->
            V.Vector 16 (E sym (BVType 8)) ->
            V.Vector 16 (E sym (BVType 8))
 vpalign i xs ys =
-  V.slice n0 n16 (V.shiftR (fromIntegral i) zero (V.append xs ys))
+  V.slice n0 n16 (V.shiftR (fromIntegral i) (bv 0) (V.append xs ys))
 
 -- | Shuffling with a mask.
 -- For more info, see `VPSHUFD` Intel instruction.
@@ -97,7 +114,57 @@ shuffle w = V.shuffle getField
                    r = fromIntegral r'
                in 4 * d + fromIntegral ((w `shiftR` (2 * r)) .&. 0x03)
 
+shufb :: V.Vector 16 (E sym (BVType 8)) ->
+         V.Vector 16 (E sym (BVType 8)) ->
+         V.Vector 16 (E sym (BVType 8))
+shufb xs is = fmap lkp is
+  where
+  lkp i = app (BVIte (bvTestBit i 7) knownNat
+              (bv 0)
+              (bvLookup xs (app $ BVTrunc n4 knownNat i)))
 
+--------------------------------------------------------------------------------
+vecOp1 :: (IsSymInterface sym, 1 <= c) =>
+  sym         {- ^ Simulator -} ->
+  Endian      {- ^ How to split-up the bit-vector -} ->
+  NatRepr w   {- ^ Total width of the bit-vector -} ->
+  NatRepr c   {- ^ Width of individual elements -} ->
+  AtomWrapper (RegEntry sym) (M.BVType w) {- ^ The input value -} ->
+  (forall n. (1 <= n, (n * c) ~ w) =>
+     V.Vector n (E sym (BVType c)) -> V.Vector n (E sym (BVType c))) ->
+  -- ^ Definition of operation
+  IO (RegValue sym (BVType w)) -- ^ The final result.
+vecOp1 sym endian totLen elLen x f =
+  unpack endian totLen elLen x $ \v ->
+  evalE sym (V.toBV endian elLen (f v))
+
+
+vecOp2 :: (IsSymInterface sym, 1 <= c) =>
+  sym         {- ^ Simulator -} ->
+  Endian      {- ^ How to split-up the bit-vector -} ->
+  NatRepr w   {- ^ Total width of the bit-vector -} ->
+  NatRepr c   {- ^ Width of individual elements -} ->
+  AtomWrapper (RegEntry sym) (M.BVType w) {- ^ Input value 1 -} ->
+  AtomWrapper (RegEntry sym) (M.BVType w) {- ^ Input value 2 -} ->
+  (forall n. (1 <= n, (n * c) ~ w) =>
+     V.Vector n (E sym (BVType c)) ->
+     V.Vector n (E sym (BVType c)) ->
+     V.Vector n (E sym (BVType c))) ->
+  -- ^ Definition of operation
+  IO (RegValue sym (BVType w)) -- ^ The final result.
+vecOp2 sym endian totLen elLen x y f =
+  unpack2 endian totLen elLen x y $ \u v ->
+  evalE sym (V.toBV endian elLen (f u v))
+
+
+bitOp2 :: (IsSymInterface sym) =>
+  sym                                     {- ^ The simulator -} ->
+  AtomWrapper (RegEntry sym) (M.BVType w) {- ^ Input 1 -} ->
+  AtomWrapper (RegEntry sym) (M.BVType w) {- ^ Input 2 -} ->
+  (E sym (BVType w) -> E sym (BVType w) -> App () (E sym) (BVType w)) ->
+                                          -- ^ The definition of the operation
+  IO (RegValue sym (BVType w))            {- ^ The result -}
+bitOp2 sym x y f = evalE sym $ app $ f (getVal x) (getVal y)
 
 -- | Package-up a vector expression to a bit-vector, and evaluate it.
 pack :: (IsSymInterface sym, KnownNat w, 1 <= w) =>
@@ -118,15 +185,24 @@ unpack ::
   AtomWrapper (RegEntry sym) (M.BVType w) {- ^ Input value -} ->
   (forall n. (1 <= n, (n * c) ~ w) => V.Vector n (E sym (BVType c)) -> IO a) ->
   IO a
-unpack e w c v k =
-  withDivModNat w c $ \n r ->
-    case testEquality r n0 of
-      Just Refl ->
-        case testLeq n1 n of
-          Just LeqProof -> k (V.fromBV e n c (getVal v))
-          _             -> fail "Unexpected 0 size"
-      _ -> fail ("Value not a multiple of " ++ show (widthVal c))
+unpack e w c v k = divExact w c $ \n -> k (V.fromBV e n c (getVal v))
 
+-- | Split up two bit-vectors into sub-chunks.
+unpack2 ::
+  (1 <= c) =>
+  Endian ->
+  NatRepr w ->
+  NatRepr c ->
+  AtomWrapper (RegEntry sym) (M.BVType w) ->
+  AtomWrapper (RegEntry sym) (M.BVType w) ->
+  (forall n. (1 <= n, (n * c) ~ w) =>
+      V.Vector n (E sym (BVType c)) ->
+      V.Vector n (E sym (BVType c)) ->
+      IO a) ->
+  IO a
+unpack2 e w c v1 v2 k =
+  divExact w c $ \n -> k (V.fromBV e n c (getVal v1))
+                         (V.fromBV e n c (getVal v2))
 
 
 getVal :: AtomWrapper (RegEntry sym) mt -> E sym (ToCrucibleType mt)
@@ -168,8 +244,26 @@ instance IsExpr (E sym) where
                 Val r  -> regType r
 
 
-zero :: (KnownNat w, 1 <= w) => E sym (BVType w)
-zero = app (BVLit knownNat 0)
+bv :: (KnownNat w, 1 <= w) => Int -> E sym (BVType w)
+bv i = app (BVLit knownNat (fromIntegral i))
+
+
+bvTestBit :: (KnownNat w, 1 <= w) => E sym (BVType w) -> Int -> E sym BoolType
+bvTestBit e n = app $ BVNonzero knownNat $
+                app $ BVAnd knownNat e (bv (shiftL 1 n))
+
+bvLookup ::
+  (1 <= w, KnownNat w) =>
+  V.Vector 16 (E sym (BVType w)) ->
+  E sym (BVType 4) ->
+  E sym (BVType w)
+bvLookup xs ind = ite 0 3
+  where
+  ite i b = if b < 0
+                then V.elemAtUnsafe i xs
+                else app $ BVIte (bvTestBit ind b) knownNat
+                                 (ite (2 * i + 1) (b - 1))
+                                 (ite (2 * i)     (b - 1))
 
 --------------------------------------------------------------------------------
 
@@ -178,6 +272,12 @@ n0 = knownNat
 
 n1 :: NatRepr 1
 n1 = knownNat
+
+n2 :: NatRepr 2
+n2 = knownNat
+
+n4 :: NatRepr 4
+n4 = knownNat
 
 n8 :: NatRepr 8
 n8 = knownNat
