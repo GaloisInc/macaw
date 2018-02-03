d5292afc76973cc589c47d7e09f4652d422890a6
Kevin Quick <kquick@galois.com>
2018-02-02 16:46:47 -0800

    [arm] add missing import for Operand support with genInstruction.

--- macaw-arm/src/Data/Macaw/ARM/Semantics/ARMSemantics.hs
+++ macaw-arm/src/Data/Macaw/ARM/Semantics/ARMSemantics.hs
@@ -10,24 +10,25 @@ module Data.Macaw.ARM.Semantics.ARMSemantics
     )
     where
 
-import           Data.Macaw.ARM.Arch ( armInstructionMatcher )
 import           Data.Macaw.ARM.ARMReg ( locToRegTH )
+import           Data.Macaw.ARM.Arch ( armInstructionMatcher )
+import           Data.Macaw.ARM.Operand
 import           Data.Macaw.ARM.Semantics.TH ( armAppEvaluator, armNonceAppEval )
 import qualified Data.Macaw.CFG as MC
 import           Data.Macaw.SemMC.Generator ( Generator )
 import           Data.Macaw.SemMC.TH ( genExecInstruction )
 import qualified Data.Macaw.Types as MT
 import           Data.Proxy ( Proxy(..) )
-import qualified Dismantle.ARM as D
+import           Dismantle.ARM -- must be present to supply definitions for genExecInstruction output
 import           SemMC.ARM ( ARM, Instruction )
-import           SemMC.Architecture.ARM.Opcodes ( allSemantics, allOpcodeInfo )
+import           SemMC.Architecture.ARM.Opcodes ( allA32Semantics, allA32OpcodeInfo )
 
 
-execInstruction :: MC.Value ARM ids (MT.BVType 32) -> D.Instruction -> Maybe (Generator ARM ids s ())
+execInstruction :: MC.Value ARM ids (MT.BVType 32) -> Dismantle.ARM.Instruction -> Maybe (Generator ARM ids s ())
 execInstruction = $(genExecInstruction (Proxy @ARM)
-                                           (locToRegTH (Proxy @ARM))
-                                           armNonceAppEval
-                                           armAppEvaluator
-                                           'armInstructionMatcher
-                                            allSemantics
-                                            allOpcodeInfo)
+                    (locToRegTH (Proxy @ARM))
+                    armNonceAppEval
+                    armAppEvaluator
+                    'armInstructionMatcher
+                     allA32Semantics
+                     allA32OpcodeInfo)
