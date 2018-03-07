9f66c7a773d8c29e4c887aba574db3678e95b281
Kevin Quick <kquick@galois.com>
2018-03-07 00:30:15 -0800

    [arm] Update A32 instruction generation for new type args.

--- macaw-arm/src/Data/Macaw/ARM/Semantics/ARMSemantics.hs
+++ macaw-arm/src/Data/Macaw/ARM/Semantics/ARMSemantics.hs
@@ -11,7 +11,7 @@ module Data.Macaw.ARM.Semantics.ARMSemantics
     where
 
 import           Data.Macaw.ARM.ARMReg ( locToRegTH )
-import           Data.Macaw.ARM.Arch ( armInstructionMatcher )
+import           Data.Macaw.ARM.Arch ( a32InstructionMatcher )
 import           Data.Macaw.ARM.Operand
 import           Data.Macaw.ARM.Semantics.TH ( armAppEvaluator, armNonceAppEval )
 import qualified Data.Macaw.CFG as MC
@@ -20,15 +20,19 @@ import           Data.Macaw.SemMC.TH ( genExecInstructionLogStdErr )
 import qualified Data.Macaw.Types as MT
 import           Data.Proxy ( Proxy(..) )
 import           Dismantle.ARM -- must be present to supply definitions for genExecInstruction output
-import           SemMC.ARM ( ARM, Instruction )
+import qualified SemMC.ARM as ARMSem
 import           SemMC.Architecture.ARM.Opcodes ( allA32Semantics, allA32OpcodeInfo )
 
 
-execInstruction :: MC.Value ARM ids (MT.BVType 32) -> Dismantle.ARM.Instruction -> Maybe (Generator ARM ids s ())
-execInstruction = $(genExecInstructionLogStdErr (Proxy @ARM)
-                    (locToRegTH (Proxy @ARM))
+execInstruction :: MC.Value ARMSem.ARM ids (MT.BVType 32)
+                -> Dismantle.ARM.Instruction
+                -> Maybe (Generator ARMSem.ARM ids s ())
+execInstruction = $(genExecInstructionLogStdErr (Proxy @ARMSem.ARM)
+                    (locToRegTH (Proxy @ARMSem.ARM))
                     armNonceAppEval
                     armAppEvaluator
-                    'armInstructionMatcher
+                    'a32InstructionMatcher
                     allA32Semantics
-                    allA32OpcodeInfo)
+                    allA32OpcodeInfo
+                    ([t| Dismantle.ARM.Operand |], [t| ARMSem.ARM |])
+                   )
