7cbd23dbf80cf7b6e5d770995b91e5f37284f7cb
Tristan Ravitch <tristan@nochair.net>
2018-05-20 21:41:21 -0700

    Add a generic interface for finding the entry points of a binary
    Includes implementations for x86 and PowerPC 64/32

--- macaw-semmc/src/Data/Macaw/BinaryLoader.hs
+++ macaw-semmc/src/Data/Macaw/BinaryLoader.hs
@@ -11,6 +11,7 @@ module Data.Macaw.BinaryLoader (
 
 import qualified Control.Monad.Catch as X
 import qualified Data.ElfEdit as E
+import qualified Data.List.NonEmpty as NEL
 import qualified Data.Macaw.CFG as MC
 import qualified Data.Macaw.Memory.LoadCommon as LC
 import qualified Data.Parameterized.Classes as PC
@@ -27,17 +28,30 @@ instance PC.TestEquality BinaryRepr where
 
 data LoadedBinary arch binFmt =
   LoadedBinary { memoryImage :: MC.Memory (MC.ArchAddrWidth arch)
-               , archBinaryData :: ArchBinaryData arch
-               , binaryFormatData :: BinaryFormatData binFmt
-               , loadDiagnostics :: [Diagnostic binFmt]
+               , archBinaryData :: ArchBinaryData arch binFmt
+               , binaryFormatData :: BinaryFormatData arch binFmt
+               , loadDiagnostics :: [Diagnostic arch binFmt]
                , binaryRepr :: BinaryRepr binFmt
                }
 
+-- | A class for architecture and binary container independent binary loading
+--
+-- An instance is required for every arch/format pair, but the interface is more
+-- accessible to callers than some alternatives.
 class BinaryLoader arch binFmt where
-  type ArchBinaryData arch :: *
-  type BinaryFormatData binFmt :: *
-  type Diagnostic binFmt :: *
+  -- | Architecture-specific information extracted from the binary
+  type ArchBinaryData arch binFmt :: *
+  -- | Information specific to the binary format that might be used later.
+  type BinaryFormatData arch binFmt :: *
+  type Diagnostic arch binFmt :: *
+  -- | A loader for the given binary format at a caller-specified architecture
   loadBinary :: (X.MonadThrow m) => LC.LoadOptions -> binFmt -> m (LoadedBinary arch binFmt)
+  -- | An architecture-specific function to return the entry points of a binary
+  --
+  -- This function is allowed (and encouraged) to find all possible entry points
+  -- based on the metadata available in a binary.
+  entryPoints :: (X.MonadThrow m) => LoadedBinary arch binFmt -> m (NEL.NonEmpty (MC.MemSegmentOff (MC.ArchAddrWidth arch)))
 
+-- | Return a runtime representative of the pointer width of the architecture
 addressWidth :: LoadedBinary arch binFmt -> NR.NatRepr (MC.ArchAddrWidth arch)
 addressWidth = MC.memWidth . memoryImage
--- macaw-semmc/src/Data/Macaw/BinaryLoader/X86.hs
+++ macaw-semmc/src/Data/Macaw/BinaryLoader/X86.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -9,17 +11,34 @@ module Data.Macaw.BinaryLoader.X86 (
 
 import qualified Control.Monad.Catch as X
 import qualified Data.ElfEdit as E
+import qualified Data.List.NonEmpty as NEL
 import qualified Data.Macaw.BinaryLoader as BL
+import qualified Data.Macaw.CFG as MC
 import qualified Data.Macaw.Memory.ElfLoader as EL
 import qualified Data.Macaw.Memory.LoadCommon as LC
 
 import qualified Data.Macaw.X86 as MX
 
+data X86ElfData w = X86ElfData { elf :: E.Elf w
+                               , secIndexMap :: EL.SectionIndexMap 64
+                               }
+
 instance BL.BinaryLoader MX.X86_64 (E.Elf 64) where
-  type ArchBinaryData MX.X86_64 = ()
-  type BinaryFormatData (E.Elf 64) = EL.SectionIndexMap 64
-  type Diagnostic (E.Elf 64) = EL.MemLoadWarning
+  type ArchBinaryData MX.X86_64 (E.Elf 64) = ()
+  type BinaryFormatData MX.X86_64 (E.Elf 64) = X86ElfData 64
+  type Diagnostic MX.X86_64 (E.Elf 64) = EL.MemLoadWarning
   loadBinary = loadX86Binary
+  entryPoints = x86EntryPoints
+
+x86EntryPoints :: (X.MonadThrow m)
+               => BL.LoadedBinary MX.X86_64 (E.Elf 64)
+               -> m (NEL.NonEmpty (MC.MemSegmentOff 64))
+x86EntryPoints loadedBinary = do
+  case MC.asSegmentOff (BL.memoryImage loadedBinary) addr of
+    Just entryPoint -> return (entryPoint NEL.:| [])
+    Nothing -> X.throwM (InvalidEntryPoint addr)
+  where
+    addr = MC.absoluteAddr (MC.memWord (fromIntegral (E.elfEntry (elf (BL.binaryFormatData loadedBinary)))))
 
 loadX86Binary :: (X.MonadThrow m)
               => LC.LoadOptions
@@ -31,12 +50,17 @@ loadX86Binary lopts e = do
     Right (sim, mem, warnings) ->
       return BL.LoadedBinary { BL.memoryImage = mem
                              , BL.archBinaryData = ()
-                             , BL.binaryFormatData = sim
+                             , BL.binaryFormatData =
+                               X86ElfData { elf = e
+                                          , secIndexMap = sim
+                                          }
                              , BL.loadDiagnostics = warnings
                              , BL.binaryRepr = BL.Elf64Repr
                              }
 
 data X86LoadException = X86ElfLoadError String
-                      deriving (Show)
+                      | forall w . (MC.MemWidth w) => InvalidEntryPoint (MC.MemAddr w)
+
+deriving instance Show X86LoadException
 
 instance X.Exception X86LoadException
