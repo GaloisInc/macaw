092cbc2e26169356336a4da61c3b5b0d0753550e
Joe Hendrix <jhendrix@galois.com>
2019-04-03 16:44:00 -0700

    Minor fixes to tests

--- x86/x86_tests/test_btx.c
+++ x86/x86_tests/test_btx.c
@@ -8,23 +8,14 @@
 #include<stdlib.h>
 #include<stdarg.h>
 
+#include "utils.h"
+
 uint64_t btc64_addr_reg(uint64_t* addr, uint64_t idx);
 uint64_t btr64_addr_reg(uint64_t* addr, uint64_t idx);
 uint64_t bts64_addr_reg(uint64_t* addr, uint64_t idx);
 uint64_t bts64_addr_imm4(uint64_t* addr);
 uint64_t bts64_addr_imm68(uint64_t* addr);
 
-
-
-void my_assert(bool b, const char* fmt, ...) {
-    if (!b) {
-	va_list args;
-	va_start(args, fmt);
-	vfprintf(stderr, fmt, args);
-	va_end(args);
-	exit(-1);
-    }
-}
 
 uint64_t btc_expected(uint64_t x, uint64_t v) {
     return x ^ v;
@@ -49,9 +40,8 @@ void test_addr_reg(const char* nm, uint64_t f(uint64_t*, uint64_t), uint64_t g(u
 	    uint64_t expected = (i / 64 == j) ? g(a0, val) : a0;
 	    my_assert(a[j] == expected, "%s invalid %d %d %llx %llx\n", nm, i, j, a[j], expected);
 	}
-	uint64_t r_mask = 1;
-	uint64_t expected_r = (a0 & val) ? 1 : 0;
-	my_assert((r & r_mask) == expected_r, "Bad %lld %llx %llx\n", i, expected_r, r);
+	bool expected_r = (a0 & val) != 0;
+	my_assert(get_cf(r) == expected_r, "Bad %lld %llx %llx\n", i, expected_r, r);
 
     }
 }
