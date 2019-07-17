1dae825c7370878be6e1d07a75a864a349e0728c
Daniel Wagner <dmwit@galois.com>
2019-07-17 15:08:57 -0400

    DRY

--- x86/tests/ElfX64Linux.hs
+++ x86/tests/ElfX64Linux.hs
@@ -142,13 +142,7 @@ testDiscovery expectedFilename elf = do
 withELF :: FilePath -> (E.Elf 64 -> IO ()) -> IO ()
 withELF fp k = do
   bytes <- B.readFile fp
-  case E.parseElf bytes of
-    E.ElfHeaderError off msg ->
-      error ("Error parsing ELF header at offset " ++ show off ++ ": " ++ msg)
-    E.Elf32Res [] _e32 -> error "ELF32 is unsupported in the test suite"
-    E.Elf64Res [] e64 -> k e64
-    E.Elf32Res errs _ -> error ("Errors while parsing ELF file: " ++ show errs)
-    E.Elf64Res errs _ -> error ("Errors while parsing ELF file: " ++ show errs)
+  E.parseElfOrDie (error "ELF32 is unsupported in the test suite") k bytes
 
 data ElfException = MemoryLoadError String
   deriving (Typeable, Show)
