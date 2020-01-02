8cd3732c1f50bd837f59115448a58a6a668a5ff4
Tristan Ravitch <tristan@ravit.ch>
2020-01-02 12:08:48 -0500

    Update tests and expected outputs
    I added some extra code to force the compiler to use some stack space for these
    test cases.  When the functions do not use the stack (e.g., if optimized), macaw
    identifies indirect jumps as tail calls, which is unhelpful.

--- refinement/tests/samples/address-of-label1.c
+++ refinement/tests/samples/address-of-label1.c
@@ -4,6 +4,11 @@ int64_t select(int64_t a, int64_t b)
 {
   int64_t res = 5;
   void* targets[] = {&&case1, &&case2, &&case3};
+  // This stack array (and its use later) are included to make sure that the
+  // compiler uses some stack space for this function (even when optimizing).
+  // Without stack use, macaw eagerly identifies the indirect jump as a tail
+  // call, preventing the refinement code from firing.
+  int stackArray[100];
 
   int idx = 0;
   if(a > 10)
@@ -20,7 +25,7 @@ int64_t select(int64_t a, int64_t b)
   res = res * b;
 
 
-  return res;
+  return res + stackArray[res];
 }
 
 void _start()
