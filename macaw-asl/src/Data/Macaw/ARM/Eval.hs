6cbf12623a81afd349feada6c2e1d687b290e636
Ben Selfridge <benselfridge@000279.local>
2020-03-04 11:07:45 -0800

    feature/asl: Eval compiles, probably incomplete

--- macaw-asl/src/Data/Macaw/ARM/Eval.hs
+++ macaw-asl/src/Data/Macaw/ARM/Eval.hs
@@ -23,6 +23,7 @@ import           Data.Macaw.CFG
 import qualified Data.Macaw.Memory as MM
 import           Data.Macaw.SemMC.Simplify ( simplifyValue )
 import           Data.Parameterized.Some ( Some(..) )
+import qualified Data.Parameterized.Map as MapF
 import qualified Data.Set as Set
 import           GHC.TypeLits
 
@@ -43,8 +44,11 @@ mkInitialAbsState :: (ARMArchConstraints arm, ArchStmt arm ~ ARMStmt)
                   -> ArchSegmentOff arm
                   -> MA.AbsBlockState (ArchReg arm)
 mkInitialAbsState _ _mem startAddr =
-    MA.top & MA.setAbsIP startAddr
-           & MA.absRegState . boundValue arm_LR .~ MA.ReturnAddr
+  s0 & MA.absRegState . boundValue arm_LR .~ MA.ReturnAddr
+  where initRegVals = MapF.fromList []
+        s0 = MA.fnStartAbsBlockState startAddr initRegVals []
+    -- MA.top & MA.setAbsIP startAddr
+    --        & MA.absRegState . boundValue arm_LR .~ MA.ReturnAddr
 
 
 absEvalArchFn :: (ARMArchConstraints arm)
@@ -54,8 +58,7 @@ absEvalArchFn :: (ARMArchConstraints arm)
               -> AbsValue (RegAddrWidth (ArchReg arm)) tp
 absEvalArchFn _ _r f =
   case f of
-    URem {} -> MA.TopV
-
+    ARMPrimFn _ -> MA.TopV
 
 -- For now, none of the architecture-specific statements have an effect on the
 -- abstract value.
@@ -69,30 +72,31 @@ absEvalArchStmt _ s _ = s
 postARMTermStmtAbsState :: (ARMArchConstraints arm)
                         => (forall tp . ARMReg tp -> Bool)
                         -> MM.Memory (RegAddrWidth (ArchReg arm))
-                        -> AbsProcessorState ARMReg
+                        -> AbsProcessorState ARMReg ids
                         -> RegState ARMReg (Value arm ids)
                         -> ARMTermStmt ids
                         -> Maybe (MM.MemSegmentOff (RegAddrWidth (ArchReg arm)), AbsBlockState ARMReg)
 postARMTermStmtAbsState preservePred mem s0 regState stmt =
   case stmt of
-    ARMSyscall _ ->
-      case simplifyValue (regState^.curIP) of
-        Just (RelocatableValue _ addr)
-          | Just nextPC <- MM.asSegmentOff mem (MM.incAddr 4 addr) -> do
-              let params = MA.CallParams { MA.postCallStackDelta = 0
-                                         , MA.preserveReg = preservePred
-                                         }
-              Just (nextPC, MA.absEvalCall params s0 regState nextPC)
-        _ -> error ("Syscall could not interpret next PC: " ++ show (regState ^. curIP))
-    ThumbSyscall _ ->
-      case simplifyValue (regState^.curIP) of
-        Just (RelocatableValue _ addr)
-          | Just nextPC <- MM.asSegmentOff mem (MM.incAddr 2 addr) -> do
-              let params = MA.CallParams { MA.postCallStackDelta = 0
-                                         , MA.preserveReg = preservePred
-                                         }
-              Just (nextPC, MA.absEvalCall params s0 regState nextPC)
-        _ -> error ("Syscall could not interpret next PC: " ++ show (regState ^. curIP))
+    ARMTermStmt -> error "no implementation for ARMTermStmt"
+    -- ARMSyscall _ ->
+    --   case simplifyValue (regState^.curIP) of
+    --     Just (RelocatableValue _ addr)
+    --       | Just nextPC <- MM.asSegmentOff mem (MM.incAddr 4 addr) -> do
+    --           let params = MA.CallParams { MA.postCallStackDelta = 0
+    --                                      , MA.preserveReg = preservePred
+    --                                      }
+    --           Just (nextPC, MA.absEvalCall params s0 regState nextPC)
+    --     _ -> error ("Syscall could not interpret next PC: " ++ show (regState ^. curIP))
+    -- ThumbSyscall _ ->
+    --   case simplifyValue (regState^.curIP) of
+    --     Just (RelocatableValue _ addr)
+    --       | Just nextPC <- MM.asSegmentOff mem (MM.incAddr 2 addr) -> do
+    --           let params = MA.CallParams { MA.postCallStackDelta = 0
+    --                                      , MA.preserveReg = preservePred
+    --                                      }
+    --           Just (nextPC, MA.absEvalCall params s0 regState nextPC)
+    --     _ -> error ("Syscall could not interpret next PC: " ++ show (regState ^. curIP))
 
 
 preserveRegAcrossSyscall :: (ArchReg arm ~ ARMReg, 1 <= RegAddrWidth ARMReg)
