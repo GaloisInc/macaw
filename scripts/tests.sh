3d4ff4b6a5abc1bd1695b994bfd651ba5418f6d6
Daniel Matichuk <dmatichuk@galois.com>
2020-03-10 21:35:30 -0700

    update test script to use asl-lite

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-./deps/dismantle/scripts/minify-asl.sh
-cd ./deps/asl-translator/
-make genarm
-cd ../../
-
-cabal v2-build --only-dependencies macaw-asl
+cabal v2-build --only-dependencies macaw-asl -f asl-lite
