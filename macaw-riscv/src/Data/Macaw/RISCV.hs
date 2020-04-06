1be72619663aaa94b5e8162312f1e6d473f9eb6c
Ben Selfridge <benselfridge@000279.local>
2020-04-06 00:56:31 -0700

    started on RISCVReg

--- macaw-riscv/src/Data/Macaw/RISCV.hs
+++ macaw-riscv/src/Data/Macaw/RISCV.hs
@@ -17,4 +17,23 @@ import qualified Data.Macaw.Architecture.Info as MI
 import qualified GRIFT.Types as GT
 
 riscv_info :: GT.RVRepr rv -> MI.ArchitectureInfo rv
-riscv_info _ = undefined
+riscv_info _ = MI.ArchitectureInfo
+  { MI.withArchConstraints = \_ -> undefined -- \x -> x
+  , MI.archAddrWidth = undefined
+  , MI.archEndianness = undefined
+  , MI.extractBlockPrecond = \_ _ -> undefined
+  , MI.initialBlockRegs = \_ _ -> undefined
+  , MI.disassembleFn = \_ _ _ _ -> undefined
+  , MI.mkInitialAbsState = \_ _ -> undefined
+  , MI.absEvalArchFn = \_ _ -> undefined
+  , MI.absEvalArchStmt = \_ _ -> undefined
+  , MI.identifyCall = \_ _ _ -> undefined
+  , MI.archCallParams = undefined
+  , MI.checkForReturnAddr = \_ _ -> undefined
+  , MI.identifyReturn = \_ _ _ -> undefined
+  , MI.rewriteArchFn = \_ -> undefined
+  , MI.rewriteArchStmt = \_ -> undefined
+  , MI.rewriteArchTermStmt = \_ -> undefined
+  , MI.archDemandContext = undefined
+  , MI.postArchTermStmtAbsState = \_ _ _ _ _ -> undefined
+  }
