f3b6b6ba4a8fa58cc6b23cc4fea663e99da4b4bc
Tristan Ravitch <tristan@ravit.ch>
2020-04-05 21:16:03 -0700

    Add a (dummy) symbolic backend for AArch32

---
+++ macaw-aarch32-symbolic/src/Data/Macaw/AArch32/Symbolic.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Data.Macaw.AArch32.Symbolic (
+
+  ) where
+
+import           Data.Kind ( Type )
+import qualified Data.Macaw.Symbolic as MS
+import qualified Data.Macaw.Symbolic.Backend as MSB
+import qualified Data.Parameterized.TraversableFC as FC
+import qualified SemMC.Architecture.AArch32 as SA
+
+import qualified Lang.Crucible.CFG.Extension as CE
+import qualified Lang.Crucible.Types as CT
+
+instance MS.ArchInfo SA.AArch32 where
+  archVals _ = Nothing
+
+data AArch32StmtExtension (f :: CT.CrucibleType -> Type) (ctp :: CT.CrucibleType) where
+
+instance FC.FunctorFC AArch32StmtExtension where
+instance FC.FoldableFC AArch32StmtExtension where
+instance FC.TraversableFC AArch32StmtExtension where
+instance CE.TypeApp AArch32StmtExtension where
+instance CE.PrettyApp AArch32StmtExtension where
+
+type instance MSB.MacawArchStmtExtension SA.AArch32 =
+  AArch32StmtExtension
