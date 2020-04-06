afdee5deb33699a61fce662a8b5c757caf56ca51
Ben Selfridge <benselfridge@000279.local>
2020-04-05 23:51:24 -0700

    stubbed out riscv_info

--- macaw-riscv/src/Data/Macaw/RISCV.hs
+++ macaw-riscv/src/Data/Macaw/RISCV.hs
@@ -8,12 +8,13 @@
 
 module Data.Macaw.RISCV (
   -- * Macaw configurations
-  -- riscv_info,
+  riscv_info,
   -- * Type-level tags
   -- * PPC Types
   ) where
 
 import qualified Data.Macaw.Architecture.Info as MI
-import qualified Data.Macaw.CFG as MC
-import qualified Data.Macaw.CFG.DemandSet as MDS
-import qualified Data.Macaw.Memory as MM
+import qualified GRIFT.Types as GT
+
+riscv_info :: GT.RVRepr rv -> MI.ArchitectureInfo rv
+riscv_info _ = undefined
