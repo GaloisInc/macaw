8bfbaa9df05186796691821504cac4480349828b
Ben Selfridge <benselfridge@000279.local>
2020-04-12 22:31:44 -0700

    stubbed out identifyReturn

--- macaw-riscv/src/Data/Macaw/RISCV/Identify.hs
+++ macaw-riscv/src/Data/Macaw/RISCV/Identify.hs
@@ -1,6 +1,7 @@
 module Data.Macaw.RISCV.Identify where
 
 import qualified GRIFT.Types as G
+import qualified Data.Macaw.AbsDomain.AbsState as MA
 import qualified Data.Macaw.CFG as MC
 import qualified Data.Macaw.Memory as MM
 import qualified Data.Sequence as Seq
@@ -10,4 +11,11 @@ riscvIdentifyCall :: G.RVRepr rv
                   -> Seq.Seq (MC.Stmt rv ids)
                   -> MC.RegState (MC.ArchReg rv) (MC.Value rv ids)
                   -> Maybe (Seq.Seq (MC.Stmt rv ids), MC.ArchSegmentOff rv)
-riscvIdentifyCall _ _ _ _ = undefined
+riscvIdentifyCall _ _ _ _ = Nothing
+
+riscvIdentifyReturn :: G.RVRepr rv
+                    -> Seq.Seq (MC.Stmt rv ids)
+                    -> MC.RegState (MC.ArchReg rv) (MC.Value rv ids)
+                    -> MA.AbsProcessorState (MC.ArchReg rv) ids
+                    -> Maybe (Seq.Seq (MC.Stmt rv ids))
+riscvIdentifyReturn _ _ _ _ = Nothing
