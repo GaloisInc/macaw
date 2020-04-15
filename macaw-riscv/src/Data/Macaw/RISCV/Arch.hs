a2a0d84dfcf60884a6111881a50ca57a08d94f4b
Ben Selfridge <benselfridge@000279.local>
2020-04-15 13:51:57 -0700

    improved docs and fixed RISCVReg bug (GP was 3 instead of 4)

--- macaw-riscv/src/Data/Macaw/RISCV/Arch.hs
+++ macaw-riscv/src/Data/Macaw/RISCV/Arch.hs
@@ -6,7 +6,15 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
-module Data.Macaw.RISCV.Arch where
+module Data.Macaw.RISCV.Arch
+  ( -- * RISC-V functions, statements, and terminators
+    -- | There are none at this time.
+    RISCVPrimFn
+  , RISCVStmt
+  , RISCVTermStmt
+  , riscvPrimFnHasSideEffects
+  , type RISCV
+  ) where
 
 import qualified Data.Kind as K
 import qualified Data.Macaw.CFG as MC
@@ -14,13 +22,15 @@ import qualified Data.Macaw.Memory as MM
 import qualified Data.Parameterized.TraversableF as F
 import qualified Data.Parameterized.TraversableFC as FC
 import qualified Data.Macaw.Types as MT
-import qualified GRIFT.Types as GT
+import qualified GRIFT.Types as G
 
-type RISCV rv = ( MM.MemWidth (GT.RVWidth rv)
+-- | Macaw-specific constraints we need for the RISC-V configuration
+-- type parameter.
+type RISCV rv = ( MM.MemWidth (G.RVWidth rv)
                 )
 
--- | RISC-V architecture-specific functions
-data RISCVPrimFn (rv :: GT.RV) (expr :: MT.Type -> K.Type) (tp :: MT.Type)
+-- | RISC-V architecture-specific functions (none)
+data RISCVPrimFn (rv :: G.RV) (expr :: MT.Type -> K.Type) (tp :: MT.Type)
 
 instance FC.FoldableFC (RISCVPrimFn rv) where
   foldMapFC _ _ = undefined
@@ -30,8 +40,8 @@ instance MC.IsArchFn (RISCVPrimFn rv) where
 
 type instance MC.ArchFn rv = RISCVPrimFn rv
 
--- | RISC-V architecture-specific statements
-data RISCVStmt (rv :: GT.RV) (expr :: MT.Type -> K.Type)
+-- | RISC-V architecture-specific statements (none)
+data RISCVStmt (rv :: G.RV) (expr :: MT.Type -> K.Type)
 
 instance F.FoldableF (RISCVStmt rv) where
   foldMapF _ _ = undefined
@@ -41,8 +51,8 @@ instance MC.IsArchStmt (RISCVStmt rv) where
 
 type instance MC.ArchStmt rv = RISCVStmt rv
 
--- | RISC-V block termination statements
-data RISCVTermStmt (rv :: GT.RV) ids
+-- | RISC-V block termination statements (none)
+data RISCVTermStmt (rv :: G.RV) ids
 
 instance MC.PrettyF (RISCVTermStmt rv) where
   prettyF = undefined
@@ -52,13 +62,15 @@ instance MC.PrettyF (RISCVTermStmt rv) where
 --   x[rd] := pc + zext(step) pc := pc +
 --   sext(imm20 << 0x1)
 -- But for now, we leave it as trivial.
-instance MC.IPAlignment (rv :: GT.RV) where
+-- | This is an orphan instance because we are reusing the 'G.RV' type
+-- from GRIFT.
+instance MC.IPAlignment (rv :: G.RV) where
   fromIPAligned = Just
   toIPAligned = id
 
-type instance MC.ArchTermStmt (rv :: GT.RV) = RISCVTermStmt rv
+type instance MC.ArchTermStmt (rv :: G.RV) = RISCVTermStmt rv
 
-type instance MC.ArchBlockPrecond (rv :: GT.RV) = ()
+type instance MC.ArchBlockPrecond (rv :: G.RV) = ()
 
 riscvPrimFnHasSideEffects :: RISCVPrimFn rv f tp -> Bool
-riscvPrimFnHasSideEffects _ = False
+riscvPrimFnHasSideEffects _ = undefined
