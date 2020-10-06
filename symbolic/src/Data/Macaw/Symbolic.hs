a595bdc6a4a343e2cabe59f951ee1945d91916d4
Daniel Matichuk <dmatichuk@galois.com>
2020-10-06 11:20:59 -0700

    fix haddocks

--- symbolic/src/Data/Macaw/Symbolic.hs
+++ symbolic/src/Data/Macaw/Symbolic.hs
@@ -1257,8 +1257,10 @@ runCodeBlock sym archFns archEval halloc (initMem,globs) lookupH toMemPred g reg
 --        -> CC.CFG (MS.MacawExt arch) blocks (MS.MacawFunctionArgs arch) (MS.MacawFunctionResult arch)
 --        -- ^ The CFG to simulate
 --        -> IO ()
--- useCFG hdlAlloc sym MS.ArchVals { MS.withArchEval = withArchEval } initialRegs initialMem globalMap lfh cfg = do
---   let ?recordLLVMAnnotation = \_ _ -> pure ()
+-- useCFG hdlAlloc sym archVals initialRegs initialMem globalMap lfh cfg = do
+--   let
+--     ?recordLLVMAnnotation = \_ _ -> pure ()
+--     withArchEval = MS.withArchEval archVals
 --   withArchEval sym $ \archEvalFns -> do
 --     let rep = CFH.handleReturnType (CC.cfgHandle cfg)
 --     memModelVar <- CLM.mkMemVar hdlAlloc
