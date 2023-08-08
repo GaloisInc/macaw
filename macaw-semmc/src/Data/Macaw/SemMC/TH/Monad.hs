2ce038c0867354083a1ffba49c9628562cd80337
Ryan Scott <rscott@galois.com>
2023-08-08 16:01:26 -0400

    Whitespace only

--- macaw-semmc/src/Data/Macaw/SemMC/TH/Monad.hs
+++ macaw-semmc/src/Data/Macaw/SemMC/TH/Monad.hs
@@ -62,8 +62,8 @@ import qualified What4.Interface as SI
 
 import qualified SemMC.Architecture.Location as L
 
-data MacawSemMC t = MacawSemMC 
-type Sym t fs = S.ExprBuilder t MacawSemMC fs 
+data MacawSemMC t = MacawSemMC
+type Sym t fs = S.ExprBuilder t MacawSemMC fs
 
 data BoundVarInterpretations arch t fs =
   BoundVarInterpretations { locVars :: Map.MapF (SI.BoundVar (Sym t fs)) (L.Location arch)
