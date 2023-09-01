0ebe111a8081f00bec2556c4f7f16d5f4dee16f3
Daniel Matichuk <dmatichuk@galois.com>
2023-09-01 12:19:34 -0700

    ARM.Identify: wrap block classifiers to maintain PSTATE_T
    This changes the final result of all block classifiers for
    ARM to ensure two properties:
      * The value of PSTATE_T is always considered unchanged
      within the same function. i.e. the value of PSTATE_T
      is always restored to its initial value for intra-block
      jumps. This ensures when calling a thumb function from
      an ARM function, the return point is decoded in ARM mode.
    
      * The low bit of a function address always determines
      whether or not it is ARM vs. Thumb mode, regardless of
      how it is discovered. i.e. thumb functions discovered
      from thumb mode still have their low bit set in the
      resulting discovery state

--- macaw-aarch32/src/Data/Macaw/ARM.hs
+++ macaw-aarch32/src/Data/Macaw/ARM.hs
@@ -15,7 +15,7 @@ import           Control.Applicative ( (<|>) )
 import           Data.Macaw.ARM.Arch
 import           Data.Macaw.ARM.Disassemble ( disassembleFn )
 import           Data.Macaw.ARM.Eval
-import           Data.Macaw.ARM.Identify ( identifyCall, identifyReturn, isReturnValue, conditionalReturnClassifier, conditionalCallClassifier )
+import           Data.Macaw.ARM.Identify ( identifyCall, identifyReturn, isReturnValue, conditionalReturnClassifier, conditionalCallClassifier, wrapClassifierForPstateT )
 import qualified Data.Macaw.ARM.ARMReg as ARMReg
 import qualified Data.Macaw.ARM.Semantics.ARMSemantics as ARMSem
 import qualified Data.Macaw.ARM.Semantics.ThumbSemantics as ThumbSem
@@ -52,7 +52,7 @@ arm_linux_info =
                         , MI.rewriteArchTermStmt = rewriteTermStmt
                         , MI.archDemandContext = archDemandContext
                         , MI.postArchTermStmtAbsState = postARMTermStmtAbsState preserveRegAcrossSyscall
-                        , MI.archClassifier = conditionalCallClassifier <|> conditionalReturnClassifier <|> MD.defaultClassifier
+                        , MI.archClassifier = wrapClassifierForPstateT (conditionalCallClassifier <|> conditionalReturnClassifier <|> MD.defaultClassifier)
                         }
 
 archDemandContext :: MDS.DemandContext ARM.AArch32
