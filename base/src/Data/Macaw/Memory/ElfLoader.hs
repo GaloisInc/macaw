e5a443a1de8b07a1187636e4563027a2cf4c520a
Daniel Matichuk <dmatichuk@galois.com>
2023-11-01 13:54:28 -0700

    add loader option to force segments to be read-only

--- base/src/Data/Macaw/Memory/ElfLoader.hs
+++ base/src/Data/Macaw/Memory/ElfLoader.hs
@@ -1323,9 +1323,11 @@ insertElfSegment :: RegionIndex
                     -- ^ Contents of elf file
                  -> Map (MemWord w) (RelocEntry (MemLoader w) w)
                     -- ^ Relocations to apply when inserting segment.
+                 -> Perm.Flags
+                    -- ^ drop these flags from the loaded segment
                  -> Elf.Phdr w
                  -> MemLoader w ()
-insertElfSegment regIdx addrOff shdrMap contents relocMap phdr = do
+insertElfSegment regIdx addrOff shdrMap contents relocMap dropFlags phdr = do
   w <- uses mlsMemory memAddrWidth
   reprConstraints w $
    when (Elf.phdrMemSize phdr > 0) $ do
@@ -1333,6 +1335,7 @@ insertElfSegment regIdx addrOff shdrMap contents relocMap phdr = do
     seg <- do
       let linkBaseOff = fromIntegral (Elf.phdrSegmentVirtAddr phdr)
       let flags = flagsForSegmentFlags (Elf.phdrSegmentFlags phdr)
+            .&. (complement dropFlags)
       let dta = slice (Elf.phdrFileRange phdr) contents
       let sz = fromIntegral $ Elf.phdrMemSize phdr
       memSegment relocMap regIdx addrOff (Just segIdx) linkBaseOff flags dta sz
@@ -1366,12 +1369,12 @@ memoryForElfSegments'
                    , SectionIndexMap w -- Section index map
                    , [MemLoadWarning] -- Warnings from load
                    )
-memoryForElfSegments' regIndex addrOff elf = memoryForElfSegments'' [] regIndex addrOff elf
+memoryForElfSegments' regIndex addrOff elf = memoryForElfSegments'' defaultLoadOptions regIndex addrOff elf
 
 -- | Load an elf file into memory by parsing segments.
 memoryForElfSegments''
   :: forall w
-  .  [Int]
+  .  LoadOptions
   -> RegionIndex
   -> Integer
   -> Elf.ElfHeaderInfo w
@@ -1379,11 +1382,13 @@ memoryForElfSegments''
                    , SectionIndexMap w -- Section index map
                    , [MemLoadWarning] -- Warnings from load
                    )
-memoryForElfSegments'' ignored_segs regIndex addrOff elf = do
+memoryForElfSegments'' opts regIndex addrOff elf = do
   let hdr = Elf.header elf
   let cl = Elf.headerClass hdr
   let w =  elfAddrWidth cl
   let contents = Elf.headerFileContents elf
+  let ignored_segs = ignoreSegments opts
+  let readonly_segs = readOnlySegments opts
   let phdrs = V.generate (fromIntegral (Elf.phdrCount elf)) $ \i ->
                 Elf.phdrByIndex elf (fromIntegral i)
 
@@ -1399,7 +1404,8 @@ memoryForElfSegments'' ignored_segs regIndex addrOff elf = do
             ]
       forM_ (zip [0..] (toList phdrs)) $ \(i, p) -> do
         when (Elf.phdrSegmentType p == Elf.PT_LOAD && (not $ elem i ignored_segs)) $ do
-          insertElfSegment regIndex addrOff intervals contents relocMap p
+          let dropFlags = if elem i readonly_segs then Perm.write else Perm.none
+          insertElfSegment regIndex addrOff intervals contents relocMap dropFlags p
 
 -- | Load an elf file into memory by parsing segments.
 memoryForElfSegments
@@ -1413,7 +1419,7 @@ memoryForElfSegments
 memoryForElfSegments opt elf = do
   let regIndex = adjustedLoadRegionIndex (Elf.headerType (Elf.header elf)) opt
   let addrOff = loadRegionBaseOffset opt
-  memoryForElfSegments'' (ignoreSegments opt) regIndex addrOff elf
+  memoryForElfSegments'' opt regIndex addrOff elf
 
 ------------------------------------------------------------------------
 -- Elf section loading
