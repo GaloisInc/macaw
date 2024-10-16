73adafd3aebccc154d8e4f4d5666269c8f88a493
Langston Barrett <langston.barrett@gmail.com>
2024-10-16 11:45:30 -0400

    dump: Fix compilation with GHC 9.4

--- macaw-aarch32-symbolic/tools/Dump.hs
+++ macaw-aarch32-symbolic/tools/Dump.hs
@@ -3,6 +3,15 @@
 
 module Main (main) where
 
+-- Sometimes, GHC 9.4 is unable to find instances of RegAddrWidth that are
+-- available by way of transitive module imports. The only reliable way of
+-- preventing this issue that I've found is to import the defining module for
+-- the instances (Data.Macaw.ARM.ARMReg) directly. See
+-- https://gitlab.haskell.org/ghc/ghc/-/issues/16234 for the upstream GHC issue.
+--
+-- This issue does not affect GHC 9.6 or later, so when we drop support for GHC
+-- 9.4, we can remove the redundant import below.
+import Data.Macaw.ARM.ARMReg ()
 import Data.Macaw.AArch32.Symbolic ()
 import Data.Macaw.ARM qualified as MA
 import Data.Macaw.Dump qualified as MD
