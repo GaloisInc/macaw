cff49453856d603aca8608f4045d4debd5280376
Your Name <you@example.com>
2024-11-13 10:11:59 -0500

    riscv-symbolic: Split 32-bit and 64-bit `dump` CLIs

--- macaw-riscv-symbolic/tools/Dump64.hs
+++ macaw-riscv-symbolic/tools/Dump64.hs
@@ -8,11 +8,12 @@ import Data.Macaw.RISCV qualified as MR
 import Data.Macaw.RISCV.Symbolic ()
 import Data.Macaw.Symbolic qualified as MS
 import Data.Proxy (Proxy(..))
+import GRIFT.Types qualified as G
 
 main :: IO ()
 main = do
   archVals <-
-    case MS.archVals (Proxy @MR.RISCV) Nothing of
+    case MS.archVals (Proxy @(MR.RISCV G.RV64GC)) Nothing of
       Just archVals -> pure archVals
       Nothing -> error "impossible"
-  MD.main MR.riscv_info archVals MR.riscvPLTStubInfo
+  MD.main (MR.riscv_info G.rv64GCRepr) archVals MR.riscvPLTStubInfo
