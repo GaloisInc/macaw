57219f73ddb77ec0fd987dde8cc464880357c0b8
Langston Barrett <langston.barrett@gmail.com>
2026-03-10 20:36:55 -0400

    symbolic: Support for more syntax formers

--- symbolic-syntax/src/Data/Macaw/Symbolic/Syntax.hs
+++ symbolic-syntax/src/Data/Macaw/Symbolic/Syntax.hs
@@ -31,11 +31,14 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Parameterized.Context as Ctx
 import qualified Data.Parameterized.NatRepr as PN
 import           Data.Parameterized.Some ( Some(..) )
-import           GHC.TypeNats ( KnownNat, type (<=) )
+import           Data.Proxy ( Proxy(..) )
+import           GHC.TypeNats ( KnownNat, type (<=), type (+) )
+import           Numeric.Natural ( Natural )
 
 import qualified Data.Macaw.CFG as DMC
 import qualified Data.Macaw.Memory as DMM
 import qualified Data.Macaw.Symbolic as DMS
+import qualified Data.Macaw.Types as DMT
 import qualified Lang.Crucible.CFG.Expr as LCE
 import qualified Lang.Crucible.CFG.Extension as LCCE
 import qualified Lang.Crucible.CFG.Reg as LCCR
@@ -186,6 +189,16 @@ extensionParser wrappers hooks =
           LCSM.depCons endianness $ \end ->
             let writeWrapper = buildPointerWriteWrapper tp end in
             go (SomeExtensionWrapper writeWrapper)
+      LCSA.AtomName "pointer-cond-read" -> do
+        LCSM.depCons LCSC.isType $ \(Some tp) -> do
+          LCSM.depCons endianness $ \end ->
+            let readWrapper = buildPointerCondReadWrapper tp end in
+            go (SomeExtensionWrapper readWrapper)
+      LCSA.AtomName "pointer-cond-write" -> do
+        LCSM.depCons LCSC.isType $ \(Some tp) ->
+          LCSM.depCons endianness $ \end ->
+            let writeWrapper = buildPointerCondWriteWrapper tp end in
+            go (SomeExtensionWrapper writeWrapper)
       LCSA.AtomName "bv-typed-literal" -> do
         -- Bitvector literals with a type argument are a special case.  We must
         -- parse the type argument separately before parsing the remaining
@@ -195,6 +208,45 @@ extensionParser wrappers hooks =
             LCT.BVRepr{} ->
               go (SomeExtensionWrapper (buildBvTypedLitWrapper tp))
             _ -> empty
+      LCSA.AtomName "overflows" -> do
+        LCSM.depCons overflowOp $ \op ->
+          LCSM.depCons LCSC.isType $ \(Some tp) ->
+            case tp of
+              LCT.BVRepr bvWidth ->
+                WI.withKnownNat bvWidth $
+                  go (SomeExtensionWrapper (wrapMacawOverflows op bvWidth))
+              _ -> empty
+      LCSA.AtomName "fresh-symbolic" -> do
+        LCSM.depCons parseMacawType $ \(Some macawTp) ->
+          go (SomeExtensionWrapper (buildMacawFreshSymbolicWrapper macawTp))
+      LCSA.AtomName "global-ptr" -> do
+        LCSM.depCons LCSC.nat $ \region ->
+          LCSM.depCons LCSC.nat $ \offset ->
+            go (SomeExtensionWrapper (buildMacawGlobalPtrWrapper (fromIntegral region) offset))
+      LCSA.AtomName "pointer-trunc" -> do
+        LCSM.depCons LCSC.isType $ \(Some srcTp) ->
+          LCSM.depCons LCSC.isType $ \(Some tgtTp) ->
+            case (srcTp, tgtTp) of
+              (LCT.BVRepr srcWidth, LCT.BVRepr tgtWidth) ->
+                case PN.testLeq (PN.addNat tgtWidth (PN.knownNat @1)) srcWidth of
+                  Just PN.LeqProof ->
+                    WI.withKnownNat srcWidth $
+                      WI.withKnownNat tgtWidth $
+                        go (SomeExtensionWrapper (buildPointerTruncWrapper srcWidth tgtWidth))
+                  Nothing -> empty
+              _ -> empty
+      LCSA.AtomName "pointer-uext" -> do
+        LCSM.depCons LCSC.isType $ \(Some srcTp) ->
+          LCSM.depCons LCSC.isType $ \(Some tgtTp) ->
+            case (srcTp, tgtTp) of
+              (LCT.BVRepr srcWidth, LCT.BVRepr tgtWidth) ->
+                case PN.testLeq (PN.addNat srcWidth (PN.knownNat @1)) tgtWidth of
+                  Just PN.LeqProof ->
+                    WI.withKnownNat srcWidth $
+                      WI.withKnownNat tgtWidth $
+                        go (SomeExtensionWrapper (buildPointerUExtWrapper srcWidth tgtWidth))
+                  Nothing -> empty
+              _ -> empty
       _ ->
         case Map.lookup name wrappers of
           Nothing -> empty
@@ -220,6 +272,15 @@ extensionParser wrappers hooks =
       case nm of
         "le" -> pure DMM.LittleEndian
         "be" -> pure DMM.BigEndian
+        _ -> empty
+
+    overflowOp = do
+      LCSA.AtomName nm <- LCSC.atomName
+      case nm of
+        "uadc" -> pure DMS.Uadc
+        "sadc" -> pure DMS.Sadc
+        "usbb" -> pure DMS.Usbb
+        "ssbb" -> pure DMS.Ssbb
         _ -> empty
 
 -- | Wrap a statement extension binary operator
@@ -624,6 +685,125 @@ pointerWrite tp endianness ptr val =
                                        val
       return (LCCR.EvalExt writeExt))
 
+-- | Wrapper for the 'DMS.MacawCondReadMem' syntax extension that enables users to
+-- conditionally read through a pointer.
+--
+-- > pointer-cond-read type endianness condition ptr default-value
+buildPointerCondReadWrapper
+  :: ( KnownNat w
+     , DMC.MemWidth w
+     , w ~ DMC.ArchAddrWidth arch
+     )
+  => LCT.TypeRepr tp
+  -> DMM.Endianness
+  -> ExtensionWrapper arch
+                      (Ctx.EmptyCtx Ctx.::> LCT.BoolType
+                                    Ctx.::> LCLM.LLVMPointerType w
+                                    Ctx.::> tp)
+                      tp
+buildPointerCondReadWrapper tp endianness = ExtensionWrapper
+  { extName = LCSA.AtomName "pointer-cond-read"
+  , extArgTypes = Ctx.empty Ctx.:> LCT.BoolRepr
+                            Ctx.:> LCLM.LLVMPointerRepr LCT.knownNat
+                            Ctx.:> tp
+  , extWrapper =
+      \args -> Ctx.uncurryAssignment (pointerCondRead tp endianness) args }
+
+-- | Conditionally read through a pointer using 'DMS.MacawCondReadMem'.
+pointerCondRead :: ( w ~ DMC.ArchAddrWidth arch
+                   , DMM.MemWidth w
+                   , KnownNat w
+                   , ExtensionParser m ext s
+                   , ext ~ DMS.MacawExt arch
+                   )
+                => LCT.TypeRepr tp
+                -> DMM.Endianness
+                -> LCCR.Atom s LCT.BoolType
+                -> LCCR.Atom s (LCLM.LLVMPointerType w)
+                -> LCCR.Atom s tp
+                -> m (LCCR.AtomValue ext s tp)
+pointerCondRead tp endianness cond ptr defVal =
+  withSupportedPointerReadWriteTypes tp
+    (\bits bytes -> do -- `Bitvector w` case
+      let readInfo = DMC.BVMemRepr bytes endianness
+      -- For bitvector case, need to convert the default value to pointer
+      defPtrAtom <- LCSC.freshAtom WP.InternalPos
+        (LCCR.EvalApp (LCE.ExtensionApp (DMS.BitsToPtr bits defVal)))
+      let readExt = DMS.MacawCondReadMem (DMC.addrWidthRepr WI.knownNat)
+                                         readInfo
+                                         cond
+                                         ptr
+                                         defPtrAtom
+      readAtom <- LCSC.freshAtom WP.InternalPos (LCCR.EvalExt readExt)
+      return (LCCR.EvalApp (LCE.ExtensionApp (DMS.PtrToBits bits readAtom))))
+    (\_bits bytes -> do -- `Pointer` case
+      let readInfo = DMC.BVMemRepr bytes endianness
+      let readExt = DMS.MacawCondReadMem (DMC.addrWidthRepr WI.knownNat)
+                                         readInfo
+                                         cond
+                                         ptr
+                                         defVal
+      return (LCCR.EvalExt readExt))
+
+-- | Wrapper for the 'DMS.MacawCondWriteMem' syntax extension that enables users to
+-- conditionally write data through a pointer.
+--
+-- > pointer-cond-write type endianness condition ptr value
+buildPointerCondWriteWrapper
+  :: ( w ~ DMC.ArchAddrWidth arch
+     , DMM.MemWidth w
+     , KnownNat w
+     , ext ~ DMS.MacawExt arch
+     )
+  => LCT.TypeRepr tp
+  -> DMM.Endianness
+  -> ExtensionWrapper arch
+                      (Ctx.EmptyCtx Ctx.::> LCT.BoolType
+                                    Ctx.::> LCLM.LLVMPointerType w
+                                    Ctx.::> tp)
+                      LCT.UnitType
+buildPointerCondWriteWrapper tp endianness = ExtensionWrapper
+  { extName = LCSA.AtomName "pointer-cond-write"
+  , extArgTypes = Ctx.empty Ctx.:> LCT.BoolRepr
+                            Ctx.:> LCLM.LLVMPointerRepr LCT.knownNat
+                            Ctx.:> tp
+  , extWrapper =
+      \args -> Ctx.uncurryAssignment (pointerCondWrite tp endianness) args }
+
+-- | Conditionally write through a pointer using 'DMS.MacawCondWriteMem'.
+pointerCondWrite :: ( w ~ DMC.ArchAddrWidth arch
+                    , DMM.MemWidth w
+                    , KnownNat w
+                    , ExtensionParser m ext s
+                    , ext ~ DMS.MacawExt arch
+                    )
+                  => LCT.TypeRepr tp
+                  -> DMM.Endianness
+                  -> LCCR.Atom s LCT.BoolType
+                  -> LCCR.Atom s (LCLM.LLVMPointerType w)
+                  -> LCCR.Atom s tp
+                  -> m (LCCR.AtomValue ext s LCT.UnitType)
+pointerCondWrite tp endianness cond ptr val =
+  withSupportedPointerReadWriteTypes tp
+    (\bits bytes -> do -- `Bitvector w` case
+      toPtrAtom <- LCSC.freshAtom WP.InternalPos
+        (LCCR.EvalApp (LCE.ExtensionApp (DMS.BitsToPtr bits val)))
+      let writeInfo = DMC.BVMemRepr bytes endianness
+      let writeExt = DMS.MacawCondWriteMem (DMC.addrWidthRepr WI.knownNat)
+                                           writeInfo
+                                           cond
+                                           ptr
+                                           toPtrAtom
+      return (LCCR.EvalExt writeExt))
+    (\_bits bytes -> do -- `Pointer` case
+      let writeInfo = DMC.BVMemRepr bytes endianness
+      let writeExt = DMS.MacawCondWriteMem (DMC.addrWidthRepr WI.knownNat)
+                                           writeInfo
+                                           cond
+                                           ptr
+                                           val
+      return (LCCR.EvalExt writeExt))
+
 -- | Wrapper for constructing bitvector literals matching the size of an
 -- 'LCT.BVRepr'.  This enables users to instantiate literals with portable
 -- types (such as SizeT).
@@ -646,6 +826,148 @@ bvTypedLit :: forall s ext w m
           -> LCCR.Atom s LCT.IntegerType
           -> m (LCCR.AtomValue ext s (LCT.BVType w))
 bvTypedLit (LCT.BVRepr w) val = return (LCCR.EvalApp (LCE.IntegerToBV w val))
+
+-- | Wrapper for 'DMS.MacawFreshSymbolic' that generates a fresh symbolic variable.
+--
+-- > fresh-symbolic type
+buildMacawFreshSymbolicWrapper
+  :: forall arch tp
+   . DMT.TypeRepr tp
+  -> ExtensionWrapper arch Ctx.EmptyCtx (DMS.ToCrucibleType tp)
+buildMacawFreshSymbolicWrapper tpRepr = ExtensionWrapper
+  { extName = LCSA.AtomName "fresh-symbolic"
+  , extArgTypes = Ctx.empty
+  , extWrapper = \_ ->
+      return (LCCR.EvalExt (DMS.MacawFreshSymbolic tpRepr))
+  }
+
+-- | Parse a Macaw type representation.
+--
+-- Supports:
+-- - MacawBool (simple atom)
+-- - (MacawBV 64) (s-expression with width)
+parseMacawType :: LCSM.MonadSyntax LCSA.Atomic m
+               => m (Some DMT.TypeRepr)
+parseMacawType = LCSM.describe "Macaw type" $
+  -- Try s-expression first: (MacawBV n)
+  (LCSM.depCons LCSC.atomName $ \name ->
+      case name of
+        LCSA.AtomName "MacawBV" ->
+          LCSM.depCons LCSC.nat $ \n ->
+            case PN.someNat n of
+              Just (Some nRepr) ->
+                case PN.isZeroOrGT1 nRepr of
+                  Left PN.Refl -> empty  -- width must be >= 1
+                  Right PN.LeqProof ->
+                    return (Some (DMT.BVTypeRepr nRepr))
+              Nothing -> empty
+        _ -> empty)
+  <|>
+  (do name <- LCSC.atomName
+      case name of
+        LCSA.AtomName "MacawBool" -> return (Some DMT.BoolTypeRepr)
+        _ -> empty)
+
+-- | Build a wrapper for 'DMS.PtrTrunc' with explicit source and target widths.
+--
+-- > pointer-trunc (Bitvector source-width) (Bitvector target-width) ptr
+buildPointerTruncWrapper
+  :: forall arch w r
+   . ( KnownNat w
+     , KnownNat r
+     , 1 <= r
+     , (r + 1) <= w
+     , 1 <= w
+     )
+  => PN.NatRepr w
+  -> PN.NatRepr r
+  -> ExtensionWrapper arch
+                      (Ctx.EmptyCtx Ctx.::> LCLM.LLVMPointerType w)
+                      (LCLM.LLVMPointerType r)
+buildPointerTruncWrapper wRepr rRepr = ExtensionWrapper
+  { extName = LCSA.AtomName "pointer-trunc"
+  , extArgTypes = Ctx.empty Ctx.:> LCLM.LLVMPointerRepr wRepr
+  , extWrapper = \args ->
+      let ptrTrunc = LCCR.EvalExt . DMS.PtrTrunc rRepr in
+      Ctx.uncurryAssignment (pure . ptrTrunc) args
+  }
+
+-- | Build a wrapper for 'DMS.PtrUExt' with explicit source and target widths.
+--
+-- > pointer-uext (Bitvector source-width) (Bitvector target-width) ptr
+buildPointerUExtWrapper
+  :: forall arch w r
+   . ( KnownNat w
+     , KnownNat r
+     , 1 <= w
+     , (w + 1) <= r
+     , 1 <= r
+     )
+  => PN.NatRepr w
+  -> PN.NatRepr r
+  -> ExtensionWrapper arch
+                      (Ctx.EmptyCtx Ctx.::> LCLM.LLVMPointerType w)
+                      (LCLM.LLVMPointerType r)
+buildPointerUExtWrapper wRepr rRepr = ExtensionWrapper
+  { extName = LCSA.AtomName "pointer-uext"
+  , extArgTypes = Ctx.empty Ctx.:> LCLM.LLVMPointerRepr wRepr
+  , extWrapper = \args ->
+      let ptrUExt = LCCR.EvalExt . DMS.PtrUExt rRepr in
+      Ctx.uncurryAssignment (pure . ptrUExt) args
+  }
+
+-- | Wrapper for 'DMS.MacawOverflows' expression extension that tests whether
+-- an arithmetic operation overflows.
+--
+-- > overflows op width lhs rhs carry
+--
+-- Where op is one of: uadc, sadc, usbb, ssbb
+wrapMacawOverflows
+  :: forall arch w
+   . ( KnownNat w
+     , 1 <= w
+     )
+  => DMS.MacawOverflowOp
+  -> PN.NatRepr w
+  -> ExtensionWrapper arch
+                      (Ctx.EmptyCtx Ctx.::> LCT.BVType w
+                                    Ctx.::> LCT.BVType w
+                                    Ctx.::> LCT.BoolType)
+                      LCT.BoolType
+wrapMacawOverflows op wRepr = ExtensionWrapper
+  { extName = LCSA.AtomName "overflows"
+  , extArgTypes = Ctx.empty Ctx.:> LCT.BVRepr wRepr
+                            Ctx.:> LCT.BVRepr wRepr
+                            Ctx.:> LCT.BoolRepr
+  , extWrapper = \args ->
+      let overflows lhs rhs carry =
+            LCCR.EvalApp (LCE.ExtensionApp (DMS.MacawOverflows op wRepr lhs rhs carry))
+      in Ctx.uncurryAssignment (\lhs rhs carry -> pure (overflows lhs rhs carry)) args }
+
+-- | Wrapper for 'DMS.MacawGlobalPtr' that converts a memory address to a pointer.
+--
+-- > global-ptr region offset
+--
+-- Takes a region index (integer) and offset (natural) and constructs a MemAddr.
+buildMacawGlobalPtrWrapper
+  :: forall arch w
+   . ( w ~ DMC.ArchAddrWidth arch
+     , KnownNat w
+     , DMC.MemWidth w
+     )
+  => Integer
+  -> Natural
+  -> ExtensionWrapper arch Ctx.EmptyCtx (LCLM.LLVMPointerType w)
+buildMacawGlobalPtrWrapper region offset = ExtensionWrapper
+  { extName = LCSA.AtomName "global-ptr"
+  , extArgTypes = Ctx.empty
+  , extWrapper = \_ ->
+      let addr = DMM.MemAddr
+            { DMM.addrBase = fromInteger region
+            , DMM.addrOffset = fromIntegral offset
+            }
+      in return (LCCR.EvalExt (DMS.MacawGlobalPtr (DMM.addrWidthRepr (Proxy :: Proxy w)) addr))
+  }
 
 -- | Syntax extension wrappers
 extensionWrappers
